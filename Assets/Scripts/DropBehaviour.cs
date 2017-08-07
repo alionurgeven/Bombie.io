37cf168b41d69f64212c65c428b985dd0e6fd058
alonge <ali.geven@ug.bilkent.edu.tr>
2017-08-07 22:21:33 +0300

    Initial commit for prototype
    Basic rigidbody and collider integration to game prototype.

---
+++ Assets/Scripts/DropBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DropBehaviour : MonoBehaviour
+{
+    public void DropItem(GameObject itemToDrop, Vector3 position, Quaternion rotation)
+    {
+        Instantiate(itemToDrop, position, rotation);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using CnControls;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private GameObject bombPrefab;
+
+    private Rigidbody2D rb2d;
+    private DropBehaviour dropBehaviour;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        dropBehaviour = GetComponent<DropBehaviour>();
+    }
+
+    private void Update()
+    {
+        float horizontal = CnInputManager.GetAxisRaw("Horizontal");
+        float vertical = CnInputManager.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(horizontal, vertical);
+        direction.Normalize();
+        direction *= movementSpeed;
+
+        rb2d.velocity = direction;
+
+        if (CnInputManager.GetButtonDown("Jump"))
+        {
+            dropBehaviour.DropItem(bombPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+
+}
