7786f4116c25856820d0142f91eca515076e99b8
Anıl Sert <anil.sert9@gmail.com>
2017-08-11 23:11:54 +0300

    Bug fix CameraBehaviour script MissingReferenceException after player is destroyed

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -6,13 +6,20 @@ public class CameraBehavior : MonoBehaviour {
 
     [SerializeField]
     private Transform Player;
-	// Update is called once per frame
-	private void Update () {
+
+    private void Update ()
+    {
         FollowPlayer();
 	}
 
     private void FollowPlayer()
     {
-        this.transform.position = Vector3.right * Player.position.x + Vector3.up * Player.position.y + Vector3.back * 10;
+        if (Player != null)
+        {
+            transform.position = 
+                Vector3.right * Player.position.x + 
+                Vector3.up * Player.position.y + 
+                Vector3.back * 10;
+        }
     }
 }
